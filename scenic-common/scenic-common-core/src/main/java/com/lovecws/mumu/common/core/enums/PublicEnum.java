157bd5ea83f1c89b2e713363fa5717f6f3c8f9d2
llmao <mario.mao@zhongkejf.com>
2017-08-09 12:24:26 +0800

    init

---
+++ scenic-common/scenic-common-core/src/main/java/com/lovecws/mumu/common/core/enums/PublicEnum.java
@@ -0,0 +1,43 @@
+package com.lovecws.mumu.common.core.enums;
+
+/**
+ * @desc 公共状态 0 分类禁用、1分类正常使用、2 分类删除
+ * @author itmaoo
+ * @version 2016年8月11日 上午9:35:53
+ */
+public enum PublicEnum {
+
+	/**
+	 * 禁用 forbit
+	 */
+	FORBIDDEN("forbit"),
+
+	/**
+	 * 正常 normal
+	 */
+	NORMAL("normal"),
+
+	/**
+	 * 删除 delete
+	 */
+	DELETE("delete");
+
+	/** 描述 */
+	private String status;
+
+	private PublicEnum(String status) {
+		this.status = status;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String value() {
+		return status;
+	}
+}
---
+++ scenic-common/scenic-common-redis/src/main/java/com/lovecws/mumu/common/redis/JedisClientStandaloneImpl.java
@@ -0,0 +1,9 @@
+package com.lovecws.mumu.common.redis;
+
+/**
+ * 单个redis服务器
+ * @author itmaoo
+ */
+public class JedisClientStandaloneImpl extends JedisClientImpl {
+
+}
