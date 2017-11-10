c5b52e7c440a8d521bc3bf0eb046519917d38295
llmao <mario.mao@zhongkejf.com>
2017-11-10 14:25:36 +0800

    Eclipse导出War可以正常运行

--- scenic-cms/scenic-cms-web/src/main/java/com/itmaoo/scenic/AppStart.java
+++ scenic-cms/scenic-cms-web/src/main/java/com/itmaoo/scenic/AppStart.java
@@ -2,9 +2,10 @@ package com.itmaoo.scenic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-
 @SpringBootApplication
 @ImportResource(locations = {"classpath*:spring-basic.xml"})
 public class AppStart extends SpringBootServletInitializer {
@@ -12,5 +13,8 @@ public class AppStart extends SpringBootServletInitializer {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(AppStart.class, args);
     }
-    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AppStart.class);
+    }
 }
